fe4e8538569719f928836dc5a6717472a758897a
MasiTah <75667885+MasiTah123@users.noreply.github.com>
2021-05-24 13:30:55 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,23 +2,26 @@
 #include <stdlib.h>
 
 void header();
-void insertmatriks(int vertex);
-void prim(int vertex);
-void kruskal(int vertex);
+void insertmatriks();
+int prim();
+void kruskal();
+void spanningTree();
 int checkifcircuit(int array[50][50], int vertex);
 void checkifconnected();
 
 int array[50][50], hubung;
+int vertex; 
+int spanning[50][50];
 
 /* Utilities Function START*/
 
-void insertmatriks(int vertex)
+void insertmatriks()
 {
     printf("Enter the adjacency matrix : \n");
-    for(int i=1; i<=vertex; i++){
-        for(int j=1; j<=vertex; j++){
+    for(int i=0; i<vertex; i++){
+        for(int j=0; j<vertex; j++){
             if(i != j){
-                printf("   vertex %d dan vertex %d   : ", i, j);
+                printf("   vertex %d dan vertex %d   : ", i+1, j+1);
                 scanf("%d", &array[i][j]);
                 if(array[i][j] != 0){
                     hubung = 1;
@@ -31,68 +34,94 @@ void insertmatriks(int vertex)
             }
         }
     }
+
+    printf("\n");
+    printf("Bentuk dalam matriks : \n");
+    for(int i=0; i<vertex; i++){
+        for(int j=0; j<vertex; j++){
+			printf("  %d", array[i][j]);
+        }
+	printf("\n");
+    }
 }
 
-void prim(int vertex){
+int prim(){
     int cost[50][50];
-    int u,v,min_distance,distance[80],from[80];
-    int visited[80],edges,min_cost,i,j,n;
-
-    for (i=0; i<vertex; i++){
-        for(j=0; j<vertex ; j++){
-            if(array[i][j] == 0){
-                cost[i][j] = 999;
-            }
-            else{
-                cost[i][j] = array[i][j];
-            }
+    int u,v,min_distance,distance[50],from[50];
+    int visited[50],no_of_edges,i,min_cost,j;
+    
+    //create cost[][] matrix,spanning[][]
+    for(i=0;i<vertex;i++)
+        for(j=0;j<vertex;j++)
+        {
+            if(array[i][j]==0)
+                cost[i][j]=9999;
+            else
+                cost[i][j]=array[i][j];
+                spanning[i][j]=0;
         }
+        
+    //initialise visited[],distance[] and from[]
+    distance[0]=0;
+    visited[0]=1;
+    
+    for(i=1;i<vertex;i++)
+    {
+        distance[i]=cost[0][i];
+        from[i]=0;
+        visited[i]=0;
     }
-
-    distance[0] = 0;
-    visited[0] = 1;
-
-    for(i=1; i<vertex; i++){
-        distance[i] = cost[0][i];
-        from[i] = 0;
-        visited[i] = 0;
-    }
-
-    min_cost = 0;
-    edges = vertex - 1; 
-
-    while(edges > 0){
-        min_distance = 999;
-        for(i=0; i<vertex; i++){
-            if(visited[i]==0 && distance[i]<min_distance)
+    
+    min_cost=0;        //cost of spanning tree
+    no_of_edges = vertex-1;        //no. of edges to be added
+    
+    while(no_of_edges>0)
+    {
+        //find the vertex at minimum distance from the tree
+        min_distance=9999;
+        for(i=1;i<vertex;i++)
+            if(visited[i]==0&&distance[i]<min_distance)
             {
                 v=i;
-                min_distance = distance[i];
+                min_distance=distance[i];
             }
-        }
-
-        u = from[v];
-
-        edges--;
-        visited[v] = 1;
-
-        for(i=1;i<vertex;i++){
+            
+        u=from[v];
+        
+        //insert the edge in spanning tree
+        spanning[u][v]=distance[v];
+        spanning[v][u]=distance[v];
+        no_of_edges--;
+        visited[v]=1;
+        
+        //updated the distance[] array
+        for(i=1;i<vertex;i++)
             if(visited[i]==0&&cost[i][v]<distance[i])
             {
                 distance[i]=cost[i][v];
                 from[i]=v;
             }
-        }
-
-        min_cost = min_cost + cost[u][v];    
+        
+        min_cost=min_cost+cost[u][v];
     }
-
-    printf("\n\nminimum spanning tree : %d", min_cost);
+    
+    return(min_cost);
 }	
 
-void kruskal(int vertex){
+void spanningTree(){
+    int i, j;
+    for(i=0;i<vertex;i++)
+    {
+        printf("\n");
+        for(j=0;j<vertex;j++){
+            printf("%d  ",spanning[i][j]);
+        }    
+    }
+}
+
+void kruskal(){
 	int awal, kecil=999, v=0;
- 	int uji[50];
+ 	int uji[vertex];
  	
  	for(int i=1; i<=vertex; i++)
  	{
@@ -212,22 +241,9 @@ int checkifcircuit(int array[50][50], int vertex)
 
 void header()
 {
-    printf("+++++++++++++++++++++++++++++++++\n");
+    printf("++++++++++++++++++++++++++++++++\n");
     printf("PROGRAM ALGORIMA PRIM DAN KRUSKAL\n");
-    printf("+++++++++++++++++++++++++++++++++\n");
-}
-
-void printmatrix(int vertex)
-{
-    int i, j;
-    for (i = 1; i <= vertex; i++)
-    {
-        for (j = 1; j <= vertex; j++)
-        {
-            printf("[%d] ", array[i][j]);
-        }
-        printf("\n");
-    }
+    printf("+++++++++++++++++++++++++++++++++");
 }
 /* Utilities Function END*/
 
@@ -235,42 +251,47 @@ void printmatrix(int vertex)
 /* Main and Menu Function */
 
 void main(){
-    int vertex, pilihan;
+    int pilihan;
+    int total_cost;
     while(1){
         system("cls");
         header();
-        printf("1. Masukkan Matriks\n");
-        printf("2. Prim's Algorithm\n");
-        printf("3. Kruskal's Algoritm\n");
+        printf("\n\n1. Masukkan Matriks");
+        printf("\n2. Prim's Algorithm");
+        printf("\n3. Kruskal's Algoritm\n\n");
         printf("Masukkan pilihan yang anda inginkan:");
         scanf("%d", &pilihan);
-        switch(pilihan)
+        switch (pilihan)
         {
         case 1:
             system("cls");
             header();
             printf("\n\nMasukkan jumlah vertex     : ");
     	    scanf("%d", &vertex);
-            insertmatriks(vertex);
-            printmatrix(vertex);
+            insertmatriks();
             checkifconnected();
             getch();
+            main();
             break;
         case 2:
             system("cls");
             header();
-            prim(vertex);
+            total_cost = prim();
+            printf("\n\nspanning tree :\n\n");
+            spanningTree();
+            printf("\n\nminimum spanning tree : %d", total_cost);
             getch();
+            main();
             break;
         case 3:
             system("cls");
             header();
-            kruskal(vertex);
+            kruskal();
             getch();
+            main();
             break;
         default:
             printf("Maaf pilihan yang anda masukkan salah");
-            getch();
             break;
         }
     }
